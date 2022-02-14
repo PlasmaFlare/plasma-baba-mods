c800735214755b6a8c1bf9a3d473a469ccb43b68
PlasmaFlare <plasmaflare137@gmail.com>
2022-02-14 01:15:59 -0500

    THIS: added "pointer nouns" tag to editor

--- Lua/th_add_to_editor.lua
+++ Lua/th_add_to_editor.lua
@@ -11,7 +11,7 @@ editor_objlist["text_this"] =
 	name = "text_this",
 	sprite_in_root = false,
 	unittype = "text",
-	tags = {"plasma's mods", "text", "abstract", "text_noun"},
+	tags = {"plasma's mods", "text", "abstract", "text_noun", "pointer nouns"},
 	tiling = 0,
 	type = 0,
 	layer = 20,
@@ -23,7 +23,7 @@ editor_objlist["text_that"] =
 	name = "text_that",
 	sprite_in_root = false,
 	unittype = "text",
-	tags = {"plasma's mods", "text", "abstract", "text_noun"},
+	tags = {"plasma's mods", "text", "abstract", "text_noun", "pointer nouns"},
 	tiling = 0,
 	type = 0,
 	layer = 20,
@@ -35,7 +35,7 @@ editor_objlist["text_these"] =
 	name = "text_these",
 	sprite_in_root = false,
 	unittype = "text",
-	tags = {"plasma's mods", "text", "abstract", "text_noun"},
+	tags = {"plasma's mods", "text", "abstract", "text_noun", "pointer nouns"},
 	tiling = 0,
 	type = 0,
 	layer = 20,
@@ -47,7 +47,7 @@ editor_objlist["text_those"] =
 	name = "text_those",
 	sprite_in_root = false,
 	unittype = "text",
-	tags = {"plasma's mods", "text", "abstract", "text_noun"},
+	tags = {"plasma's mods", "text", "abstract", "text_noun", "pointer nouns"},
 	tiling = 0,
 	type = 0,
 	layer = 20,
@@ -59,36 +59,36 @@ editor_objlist["text_block"] =
 	name = "text_block",
 	sprite_in_root = false,
 	unittype = "text",
-	tags = {"plasma's mods", "text", "abstract", "text_quality"},
+	tags = {"plasma's mods", "text", "abstract", "text_quality", "pointer nouns"},
 	tiling = -1,
 	type = 2,
 	layer = 20,
 	colour = {2, 1},
     colour_active = {2, 2},
 }
-editor_objlist["text_pass"] = 
-{
-	name = "text_pass",
-	sprite_in_root = false,
-	unittype = "text",
-	tags = {"plasma's mods", "text", "abstract", "text_quality"},
-	tiling = -1,
-	type = 2,
-	layer = 20,
-	colour = {4, 3},
-    colour_active = {4, 4},
-}
 editor_objlist["text_relay"] = 
 {
 	name = "text_relay",
 	sprite_in_root = false,
 	unittype = "text",
-	tags = {"plasma's mods", "text", "abstract", "text_quality"},
+	tags = {"plasma's mods", "text", "abstract", "text_quality", "pointer nouns"},
 	tiling = -1,
 	type = 2,
 	layer = 20,
 	colour = {5, 2},
-    colour_active = {5, 4},
+	colour_active = {5, 4},
+}
+editor_objlist["text_pass"] = 
+{
+	name = "text_pass",
+	sprite_in_root = false,
+	unittype = "text",
+	tags = {"plasma's mods", "text", "abstract", "text_quality", "pointer nouns"},
+	tiling = -1,
+	type = 2,
+	layer = 20,
+	colour = {4, 3},
+    colour_active = {4, 4},
 }
 
 formatobjlist()
